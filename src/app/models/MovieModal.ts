bce59054a2e4adc7286cbce8212fb9c55f5c3a5c
Tushar <tusharsharma2509@gmail.com>
2023-05-10 22:39:49 +0530

    Initial set up of moview grid

--- src/app/models/MovieModal.ts
+++ src/app/models/MovieModal.ts
@@ -1,4 +1,5 @@
 export interface Movie{
+    id:number,
     movieName: string,
     yearReleased:number,
     rating: number
--- src/app/movie-grid/movie-grid.component.ts
+++ src/app/movie-grid/movie-grid.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class MovieGridComponent {
 
+  moviesList = [
+    {
+      title: 'Movie 1',
+      description: 'Description for Movie 1',
+      showDescription: false
+    },
+    {
+      title: 'Movie 2',
+      description: 'Description for Movie 2',
+      showDescription: false
+    },
+    {
+      title: 'Movie 3',
+      description: 'Description for Movie 3',
+      showDescription: false
+    },
+    // Add more movies to the list
+  ];
+  toggleDescription(movie: any) {
+    movie.showDescription = !movie.showDescription;
+  }
 }
